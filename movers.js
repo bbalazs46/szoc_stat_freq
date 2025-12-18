80b3d6e750fdc14c38b216ba456ab0d78001f207
bbalazs46 <bender.balazs.2@gmail.com>
2025-12-18 20:22:27 +0100

    Update color values in createMovers function

--- movers.js
+++ movers.js
@@ -1,9 +1,9 @@
 import { MOVER_X_OFFSET } from './constants.js';
 
 export const createMovers = (samples) => ([
-  { amp: 50, freq: 1.1, speed: 90, phase: 0, color: [1, 0.2, 0.2, 1], flat: new Float32Array(samples * 2) },
-  { amp: 70, freq: 0.9, speed: 70, phase: 1.2, color: [0.2, 0.6, 1, 1], flat: new Float32Array(samples * 2) },
-  { amp: 60, freq: 1.4, speed: 110, phase: -0.8, color: [0.1, 0.9, 0.5, 1], flat: new Float32Array(samples * 2) }
+  { amp: 50, freq: 1.1, speed: 90, phase: 0, color: [0.5, 0.1, 0.1, 1], flat: new Float32Array(samples * 2) },
+  { amp: 70, freq: 0.9, speed: 70, phase: 1.2, color: [0.1, 0.3, 0.5, 1], flat: new Float32Array(samples * 2) },
+  { amp: 60, freq: 1.4, speed: 110, phase: -0.8, color: [0.05, 0.45, 0.25, 1], flat: new Float32Array(samples * 2) }
 ]);
 
 export const evalMover = (mover, t) => ({
